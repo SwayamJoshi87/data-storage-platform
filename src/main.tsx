230b12f106512141ac3ec9e0e9424f8559f9a93d
swayam.joshi <swayam.joshi@fdmgroup.com>
2026-05-01 00:47:42 -0400

    fix: type amplify outputs config

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import { Amplify } from 'aws-amplify';
+import type { ResourcesConfig } from 'aws-amplify';
 import config from '../amplify_outputs.json';
 import './index.css';
 import App from './App.tsx';
 
-Amplify.configure(config);
+Amplify.configure(config as ResourcesConfig);
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,5 +23,5 @@ createRoot(document.getElementById('root')!).render(
         <App />
       </ThemeProvider>
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
